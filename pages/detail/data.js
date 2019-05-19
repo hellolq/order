33f4feab69e694ea1e392eccdcb7230715a25e67
hellolq <liqiaoyes@qq.com>
2019-05-19 16:26:32 +0800

    first commit

---
+++ pages/detail/data.js
@@ -0,0 +1,15 @@
+let main_img_list =[ 'http://img.alicdn.com/imgextra/i3/3045163376/O1CN013Ow7UG1aoFdYNCQR2_!!3045163376.jpg_760x760Q50s50.jpg_.webp',
+'http://img.alicdn.com/imgextra/i3/3045163376/O1CN01Mc4dY71aoFcgVHtcF_!!3045163376.jpeg_760x760Q50s50.jpg_.webp',
+'http://img.alicdn.com/imgextra/i3/3045163376/O1CN01tPqTI61aoFchu0aGf_!!3045163376.jpeg_760x760Q50s50.jpg_.webp'
+];
+
+let detail_img_list=[
+  'http://img.alicdn.com/imgextra/i4/2200679346283/O1CN01FUPYIV1wHf0VEC3CE_!!2200679346283-0-scmitem6000.jpg_2200x2200Q90s50.jpg_.webp',
+'http://img.alicdn.com/imgextra/i1/2200679346283/O1CN01Ta4pJY1wHf0fNb9ea_!!2200679346283-0-scmitem6000.jpg_2200x2200Q90s50.jpg_.webp',
+'http://img.alicdn.com/imgextra/i4/828750028/O1CN01JNJqYC1C4rZko59pI_!!828750028-0-scmitem6000.jpg_2200x2200Q90s50.jpg_.webp',
+'http://img.alicdn.com/imgextra/i2/828750028/O1CN01lDCIgX1C4rZmvCxlq_!!828750028-0-scmitem6000.jpg_2200x2200Q90s50.jpg_.webp',
+'http://img.alicdn.com/imgextra/i4/828750028/O1CN010mTA2q1C4rZmvEN5Y_!!828750028-0-scmitem6000.jpg_2200x2200Q90s50.jpg_.webp',
+'http://img.alicdn.com/imgextra/i3/828750028/O1CN01YWYd991C4rZlO8TU6_!!828750028-0-scmitem6000.jpg_2200x2200Q90s50.jpg_.webp'
+]
+
+module.exports = { main_img_list, detail_img_list}
