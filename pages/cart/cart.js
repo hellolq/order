452549285056c87ea22f062d3e699f2c0a8730fe
hellolq <liqiaoyes@qq.com>
2019-05-20 10:34:25 +0800

    购物车

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,35 +1,18 @@
-// pages/detail/detail.js
-let imgs = require("./data.js");
+// pages/cart/cart.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    main_img_list:[],
-    goodsCount:1
+
   },
-  addGoodsCount: function(){
-     let goodsCount = ++this.data.goodsCount;
-     this.setData({
-       goodsCount: goodsCount
-     });
-  },
-  delGoodsCount(){
-    if(this.data.goodsCount > 1){
-      let goodsCount = --this.data.goodsCount;
-      this.setData({
-        goodsCount: goodsCount
-      });
-    }
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      main_img_list: imgs.main_img_list
-    });
+
   },
 
   /**
@@ -71,9 +54,7 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-     wx.switchTab({
-       url: '/pages/detail2/detail2',
-     })
+
   },
 
   /**
